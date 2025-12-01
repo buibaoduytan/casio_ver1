bd7aec13c1e82336fc00fa02eb8d538cf4d983d8
Duy tân <brodame14@gmail.com>
2025-12-01 18:33:47 +0700

    update ver 2 textbox

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ function clearDisplay() {
 function calculate() {
     try {
         display.value = eval(display.value);
-    } catch (error) {
+    } catch (error) {   
         display.value = "Error";
     }
 }
